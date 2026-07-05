59b9594fb6a2594724356d2d5a415c22b68be053
abdelhak <hako50594@gmail.com>
2026-07-05 20:13:04 +0100

    fixing some shit

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,68 +1,72 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "clangd" },
-        automatic_installation = true,
-      })
-
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      vim.lsp.config("lua_ls", { capabilities = capabilities })
-      vim.lsp.config("pyright", { capabilities = capabilities })
-      vim.lsp.config("clangd", { capabilities = capabilities })
-
-      vim.lsp.enable("lua_ls")
-      vim.lsp.enable("pyright")
-      vim.lsp.enable("clangd")
-
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-        callback = function(ev)
-          local opts = { buffer = ev.buf }
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-        end,
-      })
-    end
-  },
-
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+          "williamboman/mason.nvim",
+          "williamboman/mason-lspconfig.nvim",
+          "hrsh7th/cmp-nvim-lsp",
         },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-        })
-      })
-    end
-  }
+        config = function()
+          require("mason").setup()
+          require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "pyright", "clangd" },
+            automatic_installation = true,
+          })
+
+          local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        vim.lsp.config("lua_ls", { capabilities = capabilities })
+        vim.lsp.config("pyright", { capabilities = capabilities })
+        vim.lsp.config("clangd", {
+            capabilities = capabilities,
+            root_dir = function(filename)
+              return vim.fs.root(filename, { ".clangd", "compile_commands.json", ".git" }) or vim.uv.cwd()
+            end
+          })
+
+          vim.lsp.enable("lua_ls")
+          vim.lsp.enable("pyright")
+          vim.lsp.enable("clangd")
+
+          vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+              local opts = { buffer = ev.buf }
+              vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+              vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+              vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+            end,
+          })
+        end
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+          "hrsh7th/cmp-nvim-lsp",
+          "L3MON4D3/LuaSnip",
+          "saadparwaiz1/cmp_luasnip",
+        },
+        config = function()
+          local cmp = require("cmp")
+          local luasnip = require("luasnip")
+
+          cmp.setup({
+            snippet = {
+              expand = function(args)
+                luasnip.lsp_expand(args.body)
+              end,
+            },
+            mapping = cmp.mapping.preset.insert({
+              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            }),
+            sources = cmp.config.sources({
+              { name = 'nvim_lsp' },
+              { name = 'luasnip' },
+            })
+          })
+        end
+    }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,10 +4,29 @@ return {
           dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require('telescope.builtin')
+            local actions = require('telescope.actions')
+            local actionS = require('telescope.actions.state')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
             vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+            vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = 'Telescope git status' })
+            require('telescope').setup({
+                pickers = {
+                    git_status = {
+                        mappings = {
+                            i = {
+                                ["<C-r>"] = function(prompt_bufnr)
+                                    local selection = actionS.get_selected_entry()
+                                    vim.fn.system("git checkout -- " .. selection.value)
+                                    actions.close(prompt_bufnr)
+                                    builtin.git_status()
+                                end,
+                            },
+                        },
+                    },
+                },
+            })
         end
     },
     {
